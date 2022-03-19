85831032e10c505c8911474deb60dfc7496808d2
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-03-19 13:26:44 +0900

    stream, jdbc

--- src/poly/jdbctest/Test.java
+++ src/poly/jdbctest/Test.java
@@ -1,21 +1,23 @@
 package poly.jdbctest;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;
 
 /**
  * @author TaeWK
  */
 public class Test {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-    public static void main(String[] args) throws SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
-        String url = "jdbc:mysql://localhost:3306/jdbc";
-        String userName = "root";
+        String username = "root";
         String password = "1111";
+        String url = "jdbc:mysql://localhost:3306/contacts?serverTimezone=Asia/Seoul";
 
-        Connection connection = DriverManager.getConnection(url, userName, password);
+        Connection connection = DriverManager.getConnection(url,username,password);
+
+
 
 
     }
--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,8 +1,9 @@
 package stream;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author TaeWK
@@ -10,23 +11,45 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Map<String,String> map = new HashMap<>();
+        // 객체 배열 확인해서 걸러내기 (stream().filter(람다식 조건))
+        List<People> femaleList =
+                People
+                        .getPeopleList()
+                        .stream()
+                        .filter( (data) ->
+                                data.getGender() == 2)
+                        .collect(Collectors
+                                .toList());
+        System.out.println("Gender == 2 List ");
+        femaleList.forEach(System.out::println);
 
-        String[][] arr = {{"hello","world"}, {"hi","hello"}, {"bye","byebye"}, {"no no","yes yes"}};
 
-        for(String s[] : arr)
-            map.put(s[0], s[1]);
+        // 나이로 정렬 ( stream().sorted(Comapartor.comparing(Class::GetMethod))
+        List<People> sortedList = People
+                .getPeopleList()
+                .stream()
+                .sorted(Comparator.comparing(People::getAge))
+                .collect(Collectors.toList());
 
-        System.out.println(map.entrySet());
-        System.out.println(map.keySet());
-        System.out.println(map.values());
+        System.out.println("\nAGE ASC SORTED LIST");
+        sortedList.forEach(System.out::println);
 
-        String s = "hi";
-        String rst = "";
 
-        if(map.containsKey(s)) {
-            System.out.println(s + "가 포함됨. values -> " + map.get(s));
-        }
+        // 삼항연산자와 allMatch를 사용해서 배열안의 모든 조건이 맞는지 확인하기.
+        System.out.println("\nAGE > 10 stream, allMatch");
+        System.out.println(People.getPeopleList().stream().allMatch(
+                (data)-> data.getAge() > 10) ? "모든 사람의 나이가 11살 이상입니다." : "모든 사람의 나이가 10살 이하입니다.");
+
+        System.out.println("\nAGE > 10 stream, anyMatch");
+        System.out.println(People.getPeopleList().stream().anyMatch(
+                (data) -> data.getAge() < 10) ? "나이가 11살 미만인 사람이 한 사람도 없습니다." : "나이가 11살 이상인 사람이 한 사람 이상입니다."
+        );
+
+        // 같은 방식으로 noneMatch도 있다. -> 하나도 없을 경우 true
+        System.out.println("\nstream max로 가장 나이가 많은 사람의 이름을 가져온다.");
+        Optional<People> maxAgePeople = People.getPeopleList().stream().max(Comparator.comparing(People::getAge));
+        if(maxAgePeople.isPresent())
+            System.out.println("나이가 가장 많은 사람은 " + maxAgePeople.get().getName());
 
     }
 }
