3b6f81e2fd4a7bb151090f7297ab9ca7720ac493
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-03-20 17:28:34 +0900

    프로그래머스 2단계 "오픈채팅방" java 풀이 추가.

---
+++ src/openChat/Main.java
@@ -0,0 +1,81 @@
+package openChat;
+
+import java.util.*;
+
+
+/**
+ * @author TaeWK
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Map <uid,name>으로 하는게 편할 듯.
+        Map<String,String> map = new HashMap<>();
+        int cnt = 0;
+
+        String[] record = {
+                "Enter uid1234 Muzi",
+                "Enter uid4567 Prodo",
+                "Leave uid1234",
+                "Enter uid1234 Prodo",
+                "Change uid4567 Ryan"};
+
+        for(int i = 0 ; i < record.length; i++){
+
+
+            String[] msg = record[i].split(" ");
+
+            String move = msg[0];
+            String uid = msg[1];
+
+            String name = "";
+            if(!msg[0].equals("Leave")){
+                name = msg[2];
+            }
+
+
+
+            if(move.equals("Enter")){
+                map.put(uid,name);
+            }else if(move.equals("Leave")){
+
+            }else if(move.equals("Change")){
+                map.put(uid,name);
+                cnt++;
+            }
+        }
+
+        String[] rst = new String[record.length-cnt];
+
+        for(int i=0; i < record.length; i++){
+            String[] msg = record[i].split(" ");
+
+            String move = msg[0];
+            String uid = msg[1];
+            String name = "";
+            if(!msg[0].equals("Leave")){
+                name = msg[2];
+            }
+
+            if(move.equals("Enter")){
+                rst[i] = map.get(uid)+"님이 들어왔습니다.";
+            }else if(move.equals("Leave")){
+                rst[i] = map.get(uid)+"님이 나갔습니다.";
+            }
+        }
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(int i = 0; i < rst.length; i++){
+            if(rst[i] != null)
+                answer.add(rst[i]);
+        }
+
+
+        System.out.println(answer);
+
+        answer.toArray(new String[0]);
+
+
+    }
+}
