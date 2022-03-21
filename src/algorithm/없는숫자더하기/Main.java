336ced03b494b0ce2814e8e30298e45aed4508aa
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-03-22 06:48:52 +0900

    프로그래머스 - 없는 숫자 더하기
    stream(array).reduce(idx,(x,y) -> x + y)
    
    idx부터 하나씩 연산결과를 가지고 다시 연산 가능함.
    idx=0 이라면 array[0]부터 끝까지 연산.
    
    stream(arr).sum()) 메서드로 합계 구하기 가능.

---
+++ src/algorithm/없는숫자더하기/Main.java
@@ -0,0 +1,17 @@
+package algorithm.없는숫자더하기;
+
+import java.util.Arrays;
+
+/**
+ * @author TaeWK
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,6,7,8,0};
+
+        System.out.println(45-(Arrays.stream(numbers).reduce(0,(a, b)->  a + b)));
+
+
+    }
+}
