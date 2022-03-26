f1672807174d502d3fc8ac075ef7c72ef25f0719
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-03-26 14:05:38 +0900

    JDBC 실습

---
+++ src/poly/jdbctest/Person.java
@@ -0,0 +1,9 @@
+package poly.jdbctest;
+
+/**
+ * @author TaeWK
+ */
+public class Person {
+    private String name;
+    private int age;
+}
---
+++ src/poly/과제/Person.java
@@ -0,0 +1,19 @@
+package poly.과제;
+
+import lombok.*;
+
+/**
+ * @author TaeWK
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Person {
+
+    private String name;
+    private int age;
+
+}
