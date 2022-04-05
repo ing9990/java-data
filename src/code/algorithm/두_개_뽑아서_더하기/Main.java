bea935101fd4a99bcf7bcfa6f597595f2922304d
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-04-06 02:08:50 +0900

    프로그래머스: 두 수 뽑아서 더하기
    스택, 우선순위 큐 사용. 1ms미만 통과

---
+++ src/code/algorithm/두_개_뽑아서_더하기/Main.java
@@ -0,0 +1,32 @@
+package code.algorithm.두_개_뽑아서_더하기;
+
+import java.util.*;
+
+/**
+ * @author TaeWK
+ */
+
+public class Main {
+    //static int[] numbers = {2,1,3,4,1};
+    static int[] numbers = {5,0,2,7};
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for (int i : numbers) stack.push(i);
+
+        while(! stack.isEmpty()){
+            int last = stack.pop();
+
+            for(int i = 0 ; i < stack.size(); i++)
+                if( !queue.contains(stack.get(i) + last))
+                    queue.offer(stack.get(i) + last);
+
+        }
+
+        System.out.println(queue);
+
+    }
+}
