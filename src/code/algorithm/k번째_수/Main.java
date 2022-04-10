9643842623abf62db357e2ffec95ec5619d8fd42
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-04-10 15:20:01 +0900

    K번째 수 모듈없이 생코딩

---
+++ src/code/algorithm/k번째_수/Main.java
@@ -0,0 +1,33 @@
+package code.algorithm.k번째_수;
+
+import java.util.Arrays;
+
+/**
+ * @author TaeWK
+ */
+public class Main {
+
+    static int[] array = {1, 5, 2, 6, 3, 7, 4};
+    static int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+    public static void main(String[] args) {
+
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = splitter(array, commands[i][0], commands[i][1], commands[i][2]);
+        }
+    }
+
+    public static int splitter(int[] array, int start, int end, int k) {
+        int[] arr = new int[end - start + 1];
+
+        for (int i = start - 1 ; i < end ; i++) {
+            arr[i - start + 1] = array[i];
+        }
+
+        if(arr.length != 1) Arrays.sort(arr);
+
+        return arr[k-1];
+    }
+}
