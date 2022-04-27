c8c0a9b97d5c0ba5a1984d17aced29ea2c57bcc4
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-04-27 09:11:45 +0900

    프로그래머스 : 예상 대진표

--- src/code/algorithm/예상대진표/Main.java
+++ src/code/algorithm/예상대진표/Main.java
@@ -13,11 +13,19 @@ class Solution
         ans ++;
         return solution(n,a/2 + a%2 , b/2 + b%2);
     }
+
+    public int solution2(int n, int a, int b){
+        return Integer.toBinaryString((a-1)^(b-1)).length();
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.solution(8,4,7));
+
+        System.out.println(3^6);
+        System.out.println(Integer.toBinaryString(3^6));
+
     }
 }
---
+++ src/code/algorithm/예상대진표_XOR/Main.java
@@ -0,0 +1,15 @@
+package code.algorithm.예상대진표_XOR;
+
+/**
+ * @author TaeWK
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
