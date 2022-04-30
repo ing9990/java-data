09e09f73a980d9275b7b49012fc5de61d6ae9709
TaeWoo <gimgau0218@naver.com>
2022-05-01 02:23:56 +0900

    자바 2차원 배열 람다식 활용해서 정렬하기.

---
+++ src/code/algorithm/배열_2차원_정렬/Main.java
@@ -0,0 +1,40 @@
+package code.algorithm.배열_2차원_정렬;
+
+import java.util.Arrays;
+
+/**
+ * @author TaeWK
+ */
+public class Main {
+
+    public static void printf(int[][] graph){
+        for(int[] items : graph){
+            for (int i : items){
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] graph = {{1,2}, {3,4}, {5,6} , {7,8}};
+
+        Arrays.sort(graph,(o1,o2) -> o1[0] - o2[0] );
+        printf(graph);
+
+        System.out.println();
+
+        Arrays.sort(graph,(o1,o2) -> Integer.compare(o1[0],o2[0]) );
+        printf(graph);
+
+        System.out.println();
+        Arrays.sort(graph,(o1,o2) -> o2[1] - o1[1] );
+        printf(graph);
+
+
+
+
+
+    }
+}
