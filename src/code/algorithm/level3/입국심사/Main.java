a6c0c7e10bcae2d6094c9ba64dd89cdd57fa502f
TaeWoo <gimgau0218@naver.com>
2022-05-08 16:40:45 +0900

    프로그래머스 : 리틀 프렌즈 사천성 (미완)

---
+++ src/code/algorithm/level3/입국심사/Main.java
@@ -0,0 +1,27 @@
+package code.algorithm.level3.입국심사;
+
+/**
+ * @author TaeWK
+ */
+
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+
+        for(int i = 0; i < n; i++){
+
+        }
+
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 6;
+        int[] times = {7,10};
+
+        System.out.println(new Solution().solution(n,times));
+    }
+}
