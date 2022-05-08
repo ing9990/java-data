5c6530fd43094139d4085eea61164fd5fc7b3ad7
TaeWoo <gimgau0218@naver.com>
2022-05-08 16:40:15 +0900

    프로그래머스 : 타겟 넘버 (미완)

---
+++ src/code/algorithm/level2/타겟_넘버/Main.java
@@ -0,0 +1,22 @@
+package code.algorithm.level2.타겟_넘버;
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1,1,1,1,1};
+        int target = 3;
+
+        System.out.println(
+                new Solution().solution(numbers, target)
+        );
+
+    }
+}
