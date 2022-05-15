fc22c836215e64d81cc83222ac099231f9768a25
TaeWoo <gimgau0218@naver.com>
2022-05-16 00:00:58 +0900

    Charator Type의 '1' -> Integer Type으로 변경 예제

---
+++ src/code/algorithm/inflearn/test/charactor_int_cascade_Test/Main.java
@@ -0,0 +1,25 @@
+package code.algorithm.inflearn.test.charactor_int_cascade_Test;
+
+/**
+ * @author Taewoo
+ */
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        char c = '2';
+        int i = c - 48;
+
+        System.out.println(i);
+
+        // c = '2' (Charator)
+        // Char -> Int
+
+        // c - 48
+
+
+    }
+}
