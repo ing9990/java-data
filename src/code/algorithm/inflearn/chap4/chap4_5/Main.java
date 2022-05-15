c4b9914d31593b197e89133223495e503d0f9277
TaeWoo <gimgau0218@naver.com>
2022-05-15 20:14:15 +0900

    Hash

--- src/code/algorithm/inflearn/chap4/chap4_5/Main.java
+++ src/code/algorithm/inflearn/chap4/chap4_5/Main.java
@@ -7,29 +7,41 @@ package code.algorithm.inflearn.chap4.chap4_5;
 import java.util.*;
 
 public class Main {
+
+    public int solution(int[] arr, int n, int k) {
+
+        int answer;
+        TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                for (int l = j + 1; l < n; l++)
+                    treeSet.add(arr[i] + arr[j] + arr[l]);
+
+        int cnt = 0;
+
+        for(int i : treeSet){
+            cnt ++;
+            if(cnt == k)
+                return i;
+        }
+
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        Integer[] arr = new Integer[n];
-        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        int[] arr = new int[n];
 
-        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-        int tmp = k / n;
-        int tmp2 = k % n;
-
-        int first = arr[k / n];
-        int second = arr[k % n + tmp];
-        int third = arr[second / n + tmp + tmp2];
-
-        System.out.print(first + " ");
-        System.out.print(second + " ");
-        System.out.print(third + " " + "\n");
-
-        System.out.println(first + second + third);
+        System.out.println(new Main().solution(arr, n, k));
 
     }
 }
